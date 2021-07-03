5bb45e8cc7df6f1a1c13ef8818d4ae40f1a1e47a
DerekLeungHoWang <hkz88i00123@gmail.com>
2021-07-03 09:11:26 +0800

    update

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,4 @@
-export const API_BASE_URL = 'http://localhost:8080';
+// export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'https://ancient-headland-08346.herokuapp.com';
+
 export const ACCESS_TOKEN="access_token"
