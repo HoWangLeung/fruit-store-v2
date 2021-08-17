87af987c511d0a8b3f5594a16433e0a4e7ff76b7
DerekLeungHoWang <hkz88i00123@gmail.com>
2021-08-17 10:41:22 +0800

    social login production

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,12 +1,12 @@
- export const API_BASE_URL = process.env.NODE_ENV==='production'?
- 'https://ancient-headland-08346.herokuapp.com':
-  'http://localhost:8080'
- //https://ancient-headland-08346.herokuapp.com
+export const API_BASE_URL = process.env.NODE_ENV === 'production' ?
+    'https://ancient-headland-08346.herokuapp.com' :
+    'http://localhost:8080'
+//https://ancient-headland-08346.herokuapp.com
 
 
-export const ACCESS_TOKEN="access_token"
+export const ACCESS_TOKEN = "access_token"
 
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
+export const OAUTH2_REDIRECT_URI = process.env.NODE_ENV === 'production' ? 'https://wahkee-fruitstore.com/oauth2/redirect' : 'http://localhost:3000/oauth2/redirect'
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
