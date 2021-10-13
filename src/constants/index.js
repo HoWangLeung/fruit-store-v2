3089555f91656d67207ffa325c38947041e6cc03
Derek Leung <hkz88i00123@gmail.com>
2021-10-13 23:58:17 +0100

    fix

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,7 @@
 export const API_BASE_URL = process.env.NODE_ENV === 'production' ?
     'https://ancient-headland-08346.herokuapp.com' :
-    'http://localhost:8080'
+    'https://ancient-headland-08346.herokuapp.com'
+    //'http://localhost:8080'
 //https://ancient-headland-08346.herokuapp.com
 
 
